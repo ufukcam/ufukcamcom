e769505d6284a6f81084e3f847b8f545c6332ec8
Ufuk Çam <ufukcam07@gmail.com>
2020-09-08 23:10:31 +0300

    Update navigation.js
    gatsby link api fix

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="https://twitter.com/ufukcam">Twitter</Link>
+    <a href="https://twitter.com/ufukcam">Twitter</a>!    
     <Link to="https://github.com/ufukcam">Github</Link>
     <Link to="https://www.linkedin.com/in/ufukcam/">Linkedin</Link>
     <Link to="/contact">Contact</Link>
