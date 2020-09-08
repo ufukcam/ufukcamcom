cf7bfe1c46b12677aa7d104089ceb2e324bc2f9d
Ufuk Çam <ufukcam07@gmail.com>
2020-09-08 23:01:48 +0300

    Update navigation.js
    navi fixed

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,9 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
+    <Link to="https://twitter.com/ufukcam">Twitter</Link>
+    <Link to="https://github.com/ufukcam">Github</Link>
+    <Link to="https://www.linkedin.com/in/ufukcam/">Linkedin</Link>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
