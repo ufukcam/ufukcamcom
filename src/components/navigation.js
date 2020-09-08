d063953641477ae38aacb93c013617aaf72a6620
Ufuk Çam <ufukcam07@gmail.com>
2020-09-08 23:16:00 +0300

    Update navigation.js
    no commit

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,9 +4,6 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <a target="_blank" href="https://twitter.com/ufukcam">Twitter</a>    
-    <a target="_blank" href="https://github.com/ufukcam">Github</a>
-    <a target="_blank" href="https://www.linkedin.com/in/ufukcam/">Linkedin</a>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
