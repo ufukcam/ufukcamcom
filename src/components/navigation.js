332b92c020cabda19a4a8ce61ccb5263e1c99781
Ufuk Çam <ufukcam07@gmail.com>
2020-09-08 23:59:36 +0300

    Update navigation.js
    no commit

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,7 +6,6 @@ export default (props) => (
   <nav className="navigation"> 
     
     <Link to="/contact">Contact</Link>
-    <a target="_blank" href="https://twitter.com/ufukcam"><i class="gg-link"></i></a>
     <ThemeChanger/>
   </nav>
   
