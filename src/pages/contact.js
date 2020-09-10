125871776c5736c1f3bdec1e25caa9932ff37c80
Ufuk Çam <ufukcam07@gmail.com>
2020-09-11 00:49:54 +0300

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,7 +17,7 @@ const ContactPage = ({
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you &rarr;</p>
+          <p>Let me help you for your problems &rarr;</p>
         </div>
         <div>
           <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
