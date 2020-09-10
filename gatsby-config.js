b48d14bde363407b75fa9c09965b98ab9681cd3d
Ufuk Çam <ufukcam07@gmail.com>
2020-09-10 14:49:27 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Ufuk GatbsyJS Starter`,
+        short_name: `UfukApp`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
