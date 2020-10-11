bf114400b31eea6841faaa902068a35e66bed923
Ufuk Çam <ufukcam07@gmail.com>
2020-10-12 00:28:46 +0300

    sitemap-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,41 +42,69 @@ module.exports = {
       }
     },
     {
-    resolve: `gatsby-plugin-sitemap`,
-    options: {
-      output: `/some-other-sitemap.xml`,
-      // Exclude specific pages or groups of pages using glob parameters
-      // See: https://github.com/isaacs/minimatch
-      // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-      exclude: [`/category/*`, `/path/to/page`],
-      query: `
-        {
-          wp {
-            generalSettings {
-              siteUrl
-            }
-          }
-
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-      }`,
-      resolveSiteUrl: ({site, allSitePage}) => {
-        //Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
-        return site.wp.generalSettings.siteUrl
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `UfukApp`,
+        short_name: `UfukApp`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#381696`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
       },
-      serialize: ({ site, allSitePage }) =>
-        allSitePage.nodes.map(node => {
-          return {
-            url: `${site.wp.generalSettings.siteUrl}${node.path}`,
-            changefreq: `daily`,
-            priority: 0.7,
-          }
-        })
-    }
-  },
+    },
+    `gatsby-plugin-sass`, 
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-dark-mode',
+    // siteURL is a must for sitemap generation
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
+  ],
+}
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: require("./site-meta-data.json"),
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/_data`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
+          },
+        },
+        {
+          resolve: 'gatsby-remark-emojis',
+        }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it. replace with yours
+        trackingId: "UA-19921627-1",
+        head: true,
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -89,6 +117,12 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
+    {
+    resolve: `gatsby-plugin-sitemap`,
+    options: {
+      sitemapSize: 5000
+    }
+  },
     `gatsby-plugin-sass`, 
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
