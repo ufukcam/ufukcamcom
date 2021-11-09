bcfc53ec3940994e56303e95f92a5ceda2c0ba20
Ufuk Çam <ufukcam07@gmail.com>
2021-11-10 01:51:33 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
-      <h2>Blog Posts &darr;</h2>
+      <h2>İçerikler &darr;</h2>
       <div className="grids">
         {Posts}
       </div>
